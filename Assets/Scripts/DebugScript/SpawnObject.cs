1d3d95455173896c01f13f197f6e05ddf7d8bfd2
richardpine189 <73888797+richardpine189@users.noreply.github.com>
2022-04-02 19:08:11 -0300

    SpawningMechanic

--- Assets/Scripts/DebugScript/SpawnObject.cs
+++ Assets/Scripts/DebugScript/SpawnObject.cs
@@ -6,7 +6,12 @@ public class SpawnObject : MonoBehaviour
 {
     [SerializeField] List<GameObject> _obstacles;
     [SerializeField] List<GameObject> _pickUpObjects;
-    [SerializeField] Vector3 initialPosition;
+
+    [SerializeField] float _fallingInitialPosition;
+    [SerializeField] float _floatingInitialPosition;
+
+    private const float MIN_SPAWN_OBJECTS = -6;
+    private const float MAX_SPAWN_OBJECTS = 6;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +23,9 @@ public class SpawnObject : MonoBehaviour
     private void SimpleSpawn()
     {
         int randomObstacle = Random.Range(0, _obstacles.Count);
-        Instantiate(_obstacles[randomObstacle], initialPosition, Quaternion.identity);
+        float randomXPosition = Random.Range(MIN_SPAWN_OBJECTS, MAX_SPAWN_OBJECTS);
+        Vector3 spawnPosition = new Vector3(randomXPosition, _fallingInitialPosition, 10);
+        Instantiate(_obstacles[randomObstacle], spawnPosition, Quaternion.identity);
     }
 
 }
--- Assets/Scripts/Movement/FallingStaf.cs
+++ Assets/Scripts/Movement/FallingStaf.cs
@@ -9,6 +9,10 @@ public class FallingStaf : MonoBehaviour
     [SerializeField] private float _currentSpeed;
     [SerializeField] private float _rotationSpeed;
 
+    private const float MIN_ROTATION_SPEED = -30;
+    private const float MAX_ROTATION_SPEED = 30;
+    private const float MIN_FALLING_SPEED = 0;
+    private const float MAX_FALLING_SPEED = 30;
     private Transform _stafTransform;
     private Vector2 fallingVector;
 
@@ -18,6 +22,8 @@ public class FallingStaf : MonoBehaviour
         _stafTransform = gameObject.GetComponent<Transform>();
         
         fallingVector = isFalling ? Vector2.down : Vector2.up;
+
+        RandomizeSpeed();
     }
 
     // Update is called once per frame
@@ -25,5 +31,11 @@ public class FallingStaf : MonoBehaviour
     {
         _stafTransform.Translate(fallingVector * _initialSpeed * Time.deltaTime, Space.World);
         _stafTransform.Rotate(Vector3.forward * Time.deltaTime * _rotationSpeed);
+    }
+
+    void RandomizeSpeed()
+    {
+        _initialSpeed = Random.Range(MIN_FALLING_SPEED, MAX_FALLING_SPEED);
+        _rotationSpeed = Random.Range(MIN_ROTATION_SPEED, MAX_ROTATION_SPEED);
     }
 }
